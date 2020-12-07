65171b7c1d08e2a136a260210272fb4fd061510c
martinjohn07 <72308462+martinjohn07@users.noreply.github.com>
2020-12-07 20:14:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,53 @@
+class Box{
+    constructor(x,y,width,height) {
+
+var options= {
+    isStatic: false,
+    restitution:0.2
+    
+}
+this.visibility=255;
+this.body=Bodies.rectangle(x,y,width,height,options);
+this.width= width;
+this.height= height;
+
+World.add(world,this.body);
+    }
+
+    display() {
+       
+        if(this.body.speed<10) {
+var angle= this.body.angle;
+var pos=this.body.position;
+push ();
+translate(this.body.position.x,this.body.position.y);
+rotate(angle);
+rectMode(CENTER);
+fill ("yellow");
+rect(0,0,this.width,this.height);
+pop ();
+        }
+else{
+    World.remove(world,this.body);
+    push()
+    this.visibility=this.visibility-5;
+    tint (255,this.visibility);
+    pop();
+    
+}
+
+    }
+
+score(){
+if (this.visibility<0 && this.visibility>-105) {
+score=score+1
+}
+
+
+
+}
+
+
+
+    }
+
